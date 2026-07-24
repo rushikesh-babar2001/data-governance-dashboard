76448354ab2a2ee42cd687ba56b9345b4db8c3d4
unknown <babarrushikesh2001@gmail.com>
2026-07-24 13:41:46 +0530

    Add backend health check endpoint

---
+++ data-governance-backend/src/main/java/com/proteccio/data_governance_backend/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.proteccio.data_governance_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Data Governance Dashboard Backend is running";
+    }
+}
