8888d25e152c359994d5376236ebda4d21557d8f
unknown <babarrushikesh2001@gmail.com>
2026-09-20 16:32:15 +0530

    Test CI/CD deployment

--- data-governance-backend/src/main/java/com/proteccio/data_governance_backend/controller/DashboardController.java
+++ data-governance-backend/src/main/java/com/proteccio/data_governance_backend/controller/DashboardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/dashboard")
 @CrossOrigin(origins = "https://data-governance-dashboard-phi.vercel.app")
 public class DashboardController {
-
+   //CI/CD deployment test
     private final DashboardService dashboardService;
 
     public DashboardController(DashboardService dashboardService) {
